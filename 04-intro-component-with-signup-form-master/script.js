e45690b548239f0efce0ffa69cfb085ec11bd5f1
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-08-26 22:11:07 +0100

    All is good, except the placeholder

---
+++ 04-intro-component-with-signup-form-master/script.js
@@ -0,0 +1,33 @@
+/** @type {HTMLFormElement} */
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function (event) {
+  if (form.checkValidity()) {
+    return;
+  }
+
+  form.querySelectorAll(".form-group").forEach((formGroup) => {
+    /** @type {HTMLInputElement} */
+    const input = formGroup.querySelector(".form-group__input");
+    const { valid } = input.validity;
+
+    // Add/remove the invalid class to the form group as appropriate.
+    formGroup.classList.toggle("form-group--invalid", !valid);
+
+    // Set/remove the aria-invalid attribute as appropriate
+    input.setAttribute("aria-invalid", !valid);
+
+    // Associate the error text with the invalid input or dissociate it from it, as appropriate.
+    if (valid) {
+      input.removeAttribute("aria-describedby");
+    } else {
+      const errorText = formGroup.querySelector(".form-group__error-text");
+      input.setAttribute("aria-describedby", errorText.id);
+    }
+  });
+
+  // Focus on the first invalid input
+  form.querySelector(".form-group__input:invalid").focus();
+
+  event.preventDefault();
+});
