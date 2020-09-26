df8bffeb0eee24b1f123f5c31baab3cdd8040e72
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-09-26 20:03:01 +0100

    Init basic markup and tailwind theme

--- 08-project-tracking-intro-component-master/tailwind.config.js
+++ 08-project-tracking-intro-component-master/tailwind.config.js
@@ -1,11 +1,30 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: ["./*.html"],
   theme: {
     extend: {},
+    colors: {
+      red: "hsl(0, 100%, 68%)",
+      blue: {
+        "very-dark": "hsl(230, 29%, 20%)",
+        "gray-dark": "hsl(230, 11%, 40%)",
+        gray: "hsl(231, 7%, 65%)",
+        "gray-light": "hsl(207, 33%, 95%)",
+      },
+    },
+    fontFamily: {
+      display: ["Barlow Condensed", ...fontFamily.sans],
+      body: ["Barlow", ...fontFamily.sans],
+    },
+    fontWeight: {
+      normal: 400,
+      bold: 700,
+    },
   },
   variants: {},
   plugins: [],
