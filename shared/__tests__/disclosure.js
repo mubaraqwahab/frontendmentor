b3e8543563dbc037a4f129aae2cf7f5c6e9bc471
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-12 22:12:33 +0100

    Add stub test

---
+++ shared/__tests__/disclosure.js
@@ -0,0 +1,41 @@
+const {
+  getByLabelText,
+  getByText,
+  queryByTestId,
+  waitFor,
+} = require("@testing-library/dom");
+
+function getExampleDOM() {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <label for="username">Username</label>
+    <input id="username" />
+    <button>Print Username</button>
+  `;
+  const button = div.querySelector("button");
+  const input = div.querySelector("input");
+  button.addEventListener("click", () => {
+    setTimeout(() => {
+      const printedUsernameContainer = document.createElement("div");
+      printedUsernameContainer.innerHTML = `
+        <div data-testid="printed-username">${input.value}</div>
+      `;
+      div.appendChild(printedUsernameContainer);
+    }, Math.floor(Math.random() * 200));
+  });
+  return div;
+}
+
+test("examples of some things", async () => {
+  const famousWomanInHistory = "Ada Lovelace";
+  const container = getExampleDOM();
+
+  const input = getByLabelText(container, "Username");
+  input.value = famousWomanInHistory;
+
+  getByText(container, "Print Username").click();
+
+  await waitFor(() =>
+    expect(queryByTestId(container, "printed-username")).toBeTruthy()
+  );
+});
