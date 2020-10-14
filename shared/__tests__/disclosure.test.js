eaa518c0d993858f0e458981e9efe4e6195c66b9
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-14 23:24:17 +0100

    remove describe calls

--- shared/__tests__/disclosure.test.js
+++ shared/__tests__/disclosure.test.js
@@ -19,71 +19,64 @@ let disclosure;
 
 beforeAll(() => setupDisclosureDOM(document.body));
 
-describe("API user", () => {
-  test("can initialize a disclosure", () => {
-    const button = document.querySelector("button");
-    const content = document.querySelector("[id^='content']");
+test("can initialize a disclosure", () => {
+  const button = document.querySelector("button");
+  const content = document.querySelector("[id^='content']");
 
-    disclosure = new Disclosure(button);
-    expect(disclosure.button).toBe(button);
-    expect(disclosure.controlledElement).toBe(content);
-  });
-
-  // TODO: test cannot initialize disclosure from incomplete DOM
-
-  test("cannot initialize an out-of-sync disclosure", () => {
-    const container = document.createElement("div");
-    setupDisclosureDOM(container, false);
-    const button = container.querySelector("button");
-    expect(() => new Disclosure(button, container)).toThrow(DisclosureError);
-  });
-
-  test("can initialize multiple disclosures", () => {
-    const container = document.createElement("div");
-    setupDisclosureDOM(container, true, true);
-
-    const disclosures = Disclosure.initializeAll("button", container);
-
-    expect(disclosures).toHaveLength(2);
-    expect(disclosures[0]).toBeInstanceOf(Disclosure);
-    expect(disclosures[1]).toBeInstanceOf(Disclosure);
-  });
-
-  test("can toggle a disclosure", () => {
-    disclosure.open = true;
-    disclosure.toggle();
-    expect(disclosure.open).toBe(false);
-
-    // With `force` param
-    disclosure.toggle(false);
-    expect(disclosure.open).toBe(false);
-  });
-
-  test("can add listener", () => {
-    const mockCallback = jest.fn();
-
-    disclosure.addListener(mockCallback);
-    disclosure.toggle();
-
-    expect(mockCallback).toHaveBeenCalledTimes(1);
-    expect(mockCallback).toHaveBeenCalledWith({ target: disclosure });
-  });
-
-  test("can remove listener", () => {
-    const mockCallback = jest.fn();
-
-    disclosure.addListener(mockCallback);
-    disclosure.removeListener(mockCallback);
-    disclosure.toggle();
-
-    expect(mockCallback).toHaveBeenCalledTimes(0);
-  });
+  disclosure = new Disclosure(button);
+  expect(disclosure.button).toBe(button);
+  expect(disclosure.controlledElement).toBe(content);
 });
 
-describe("Browser user", () => {
-  test("can toggle a disclosure", () => {
-    disclosure.open = false;
-    getByText(document, /click/i).click();
-    expect(disclosure.open).toBe(true);
-  });
+// TODO: test cannot initialize disclosure from incomplete DOM
+
+test("cannot initialize an out-of-sync disclosure", () => {
+  const container = document.createElement("div");
+  setupDisclosureDOM(container, false);
+  const button = container.querySelector("button");
+  expect(() => new Disclosure(button, container)).toThrow(DisclosureError);
+});
+
+test("can initialize multiple disclosures", () => {
+  const container = document.createElement("div");
+  setupDisclosureDOM(container, true, true);
+
+  const disclosures = Disclosure.initializeAll("button", container);
+
+  expect(disclosures).toHaveLength(2);
+  expect(disclosures[0]).toBeInstanceOf(Disclosure);
+  expect(disclosures[1]).toBeInstanceOf(Disclosure);
+});
+
+test("can toggle a disclosure", () => {
+  disclosure.open = true;
+  disclosure.toggle();
+  expect(disclosure.open).toBe(false);
+
+  // With `force` param
+  disclosure.toggle(false);
+  expect(disclosure.open).toBe(false);
+
+  getByText(document, /click/i).click();
+  expect(disclosure.open).toBe(true);
+});
+
+test("can add listener", () => {
+  const mockCallback = jest.fn();
+
+  disclosure.addListener(mockCallback);
+  disclosure.toggle();
+
+  expect(mockCallback).toHaveBeenCalledTimes(1);
+  expect(mockCallback).toHaveBeenCalledWith({ target: disclosure });
+});
+
+test("can remove listener", () => {
+  const mockCallback = jest.fn();
+
+  disclosure.addListener(mockCallback);
+  disclosure.removeListener(mockCallback);
+  disclosure.toggle();
+
+  expect(mockCallback).toHaveBeenCalledTimes(0);
 });
