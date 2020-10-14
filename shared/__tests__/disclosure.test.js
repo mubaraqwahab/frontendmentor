091e0d33c5563ea849efccbb93a2208bdf7fd006
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-15 00:24:14 +0100

    Refactor test and use imports instead of require

--- shared/__tests__/disclosure.test.js
+++ shared/__tests__/disclosure.test.js
@@ -1,47 +1,83 @@
-const { beforeAll, expect, test } = require("@jest/globals");
-const { getByText } = require("@testing-library/dom");
-const { default: Disclosure, DisclosureError } = require("../disclosure.js");
+import { expect, test } from "@jest/globals";
+import { getByText } from "@testing-library/dom";
+import Disclosure, { DisclosureError } from "../disclosure.js";
 
-function setupDisclosureDOM(container, sync = true, multiple = false) {
-  const template = (n) => `
-    <button aria-expanded="${!sync}" aria-controls="content${n}" data-hidden-class="hidden">
+function setup({ inSync = true } = {}) {
+  const container = document.createElement("div");
+  container.innerHTML = `
+    <button aria-expanded="${!inSync}" aria-controls="content" data-hidden-class="hidden">
       Click me!
     </button>
-    <div id="content${n}" class="hidden">
+    <div id="content" class="hidden">
       You're seeing the disclosure content
     </div>
   `;
-  container.innerHTML = multiple ? template(1) + template(2) : template(1);
+  const button = container.querySelector("button");
+  const disclosure = new Disclosure(button, container);
+
+  return {
+    container,
+    button,
+    disclosure,
+  };
 }
 
-/** @type {Disclosure} */
-let disclosure;
-
-beforeAll(() => setupDisclosureDOM(document.body));
-
 test("can initialize a disclosure", () => {
-  const button = document.querySelector("button");
-  const content = document.querySelector("[id^='content']");
+  const { container, button, disclosure } = setup();
+  const content = container.querySelector("[id^='content']");
 
-  disclosure = new Disclosure(button);
   expect(disclosure.button).toBe(button);
   expect(disclosure.controlledElement).toBe(content);
 });
 
-// TODO: test cannot initialize disclosure from incomplete DOM
+// TODO: test cannot initialize disclosure from incomplete DOM i.e.: when:
+// aria-expanded or aria-controls or data-hidden-class, or controlled elem is missing
 
 test("cannot initialize an out-of-sync disclosure", () => {
-  const container = document.createElement("div");
-  setupDisclosureDOM(container, false);
-  const button = container.querySelector("button");
-  expect(() => new Disclosure(button, container)).toThrow(DisclosureError);
+  expect(() => setup({ inSync: false })).toThrow(DisclosureError);
 });
 
 test("can initialize multiple disclosures", () => {
   const container = document.createElement("div");
-  setupDisclosureDOM(container, true, true);
+  container.innerHTML = `
+    <button
+      aria-expanded="false"
+      data-disclosure-btn
+      aria-controls="content1"
+      data-hidden-class="hidden"
+    >
+      Click me!
+    </button>
+    <div id="content1" class="hidden">
+      You're seeing the disclosure content
+    </div>
 
-  const disclosures = Disclosure.initializeAll(container, "button");
+    <button
+      aria-expanded="true"
+      data-disclosure-btn
+      aria-controls="content2"
+      data-hidden-class="hidden"
+    >
+      Click me!
+    </button>
+    <div id="content2">
+      You're seeing the disclosure content
+    </div>
+
+    <!-- Missing data-disclosure attribute. -->
+    <button
+      aria-expanded="true"
+      aria-controls="content3"
+      data-hidden-class="hidden"
+    >
+      Click me!
+    </button>
+    <div id="content3">
+      You're seeing the disclosure content
+    </div>
+  `;
+
+  const disclosures = Disclosure.initializeAll(container);
 
   expect(disclosures).toHaveLength(2);
   expect(disclosures[0]).toBeInstanceOf(Disclosure);
@@ -49,6 +85,8 @@ test("can initialize multiple disclosures", () => {
 });
 
 test("can toggle a disclosure", () => {
+  const { disclosure, container } = setup();
+
   disclosure.open = true;
   disclosure.toggle();
   expect(disclosure.open).toBe(false);
@@ -57,11 +95,12 @@ test("can toggle a disclosure", () => {
   disclosure.toggle(false);
   expect(disclosure.open).toBe(false);
 
-  getByText(document, /click/i).click();
+  getByText(container, /click/i).click();
   expect(disclosure.open).toBe(true);
 });
 
 test("can add listener", () => {
+  const { disclosure } = setup();
   const mockCallback = jest.fn();
 
   disclosure.addListener(mockCallback);
@@ -72,6 +111,7 @@ test("can add listener", () => {
 });
 
 test("can remove listener", () => {
+  const { disclosure } = setup();
   const mockCallback = jest.fn();
 
   disclosure.addListener(mockCallback);
