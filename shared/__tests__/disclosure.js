a26cb3eebcd7c3f1ba62b92f00f34b081fc1c7ff
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-14 15:40:49 +0100

    Implement getDisclosureDOM

--- shared/__tests__/disclosure.js
+++ shared/__tests__/disclosure.js
@@ -27,10 +27,44 @@ function getExampleDOM() {
   return div;
 }
 
-describe("API user", () => {
-  test("can initialize a closed disclosure", () => {});
+function getDisclosureDOM(open) {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <button aria-expanded="${
+      "" + open
+    }" aria-controls="content" data-disclosure-btn data-hidden-class="hidden">
+      Click me!
+    </button>
+    <div id="content" class="${open ? "" : "hidden"}">
+      <p>You're seeing the disclosure content</p>
+    </div>
+  `;
+  return div;
+}
 
-  test("can initialize a open disclosure", () => {});
+console.log(getDisclosureDOM(false).innerHTML);
+
+describe("API user", () => {
+  test("can initialize a disclosure", () => {
+    // Already closed disclosure
+    // Test for open and closed disclosure
+  });
+
+  test("cannot initialize an out-of-sync disclosure", () => {});
+
+  test("can initialize multiple disclosures", () => {});
+
+  test("can toggle a disclosure", () => {});
+
+  test("can add listener", () => {
+    // Consider sync and async
+  });
+
+  test("can remove listener", () => {});
+});
+
+describe("Browser user", () => {
+  test("can toggle a disclosure", () => {});
 });
 
 test("examples of some things", async () => {
