9fc1a8df7091a8cad7f60868f2494db1286b3043
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-31 17:53:59 +0100

    Use different 'navs'. Need to change this

--- 09-room-homepage-master/tailwind.config.js
+++ 09-room-homepage-master/tailwind.config.js
@@ -1,12 +1,39 @@
+const { colors, fontFamily, spacing } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      order: {
+        "0": 0,
+      },
+    },
+    container: {
+      padding: spacing["6"],
+      center: true,
+    },
+    colors: {
+      transparent: colors.transparent,
+      white: colors.white,
+      black: colors.black,
+      gray: {
+        dark: "hsl(0, 0%, 63%)",
+        "very-dark": "hsl(0, 0%, 27%)",
+      },
+    },
+    fontFamily: {
+      sans: ["Spartan", ...fontFamily.sans],
+    },
+    fontWeight: {
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+    },
   },
   variants: {},
   plugins: [],
-}
+};
