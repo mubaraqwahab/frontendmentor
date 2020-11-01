a3afe4628ff9211b56d593a618b586db32ec5340
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-11-01 12:12:02 +0100

    Complete nav disclosure logic

--- 09-room-homepage-master/js/index.js
+++ 09-room-homepage-master/js/index.js
@@ -1,13 +1,52 @@
 import { Application, Controller } from "stimulus";
+import Disclosure from "../../shared/disclosure";
 
-class DisclosureController extends Controller {
+class NavigationController extends Controller {
+  static targets = ["button", "menuIcon", "closeIcon", "body"];
+
   connect() {
-    console.log("Hey, Stimulus");
+    this.disclosure = new Disclosure(this.buttonTarget);
+
+    // Toggle between menu/close icon on nav toggle.
+    this.disclosure.addListener(({ target }) => {
+      const { hiddenClass } = target;
+      if (target.open) {
+        this.menuIconTarget.classList.add(hiddenClass);
+        this.closeIconTarget.classList.remove(hiddenClass);
+      } else {
+        this.closeIconTarget.classList.add(hiddenClass);
+        this.menuIconTarget.classList.remove(hiddenClass);
+      }
+    });
+  }
+
+  /** Close the navigation if user clicks outside it (and not the nav button) */
+  closeOnOutsideClick(e) {
+    if (
+      !this.bodyTarget.contains(e.target) &&
+      !this.buttonTarget.contains(e.target)
+    ) {
+      this.disclosure.open = false;
+    }
+  }
+
+  /** Close the navigation if the focus leaves the links within it or the button */
+  closeOnBlur(e) {
+    if (
+      !this.bodyTarget.contains(e.relatedTarget) &&
+      e.relatedTarget !== this.buttonTarget
+    ) {
+      this.disclosure.open = false;
+    }
+  }
+
+  disconnect() {
+    this.disclosure = null;
   }
 }
 
 class CarouselController extends Controller {}
 
 const application = Application.start();
-application.register("disclosure", DisclosureController);
+application.register("navigation", NavigationController);
 application.register("carousel", CarouselController);
