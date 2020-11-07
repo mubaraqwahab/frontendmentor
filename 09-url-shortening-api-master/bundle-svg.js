8d8b725fe89e0fad8c6f0786e9b1930974493a02
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-11-07 20:07:49 +0100

    Add bundle svg script and basic markup

---
+++ 09-url-shortening-api-master/bundle-svg.js
@@ -0,0 +1,19 @@
+var svgstore = require("svgstore");
+var fs = require("fs");
+var path = require("path");
+
+var imagesDir = "./images/";
+
+var sprites = fs
+  .readdirSync(imagesDir)
+  .filter((filename) => filename.endsWith(".svg"))
+  .reduce(
+    (sprites, svg) =>
+      sprites.add(
+        svg.slice(0, -4),
+        fs.readFileSync(path.join(imagesDir, svg), "utf-8")
+      ),
+    svgstore()
+  );
+
+fs.writeFileSync(path.join(imagesDir, "sprites.svg"), sprites);
--- 09-url-shortening-api-master/tailwind.config.js
+++ 09-url-shortening-api-master/tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors, fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -6,6 +8,31 @@ module.exports = {
   purge: ["./*.html"],
   theme: {
     extend: {},
+    colors: {
+      transparent: colors.transparent,
+      white: colors.white,
+      black: colors.black,
+      cyan: "hsl(180, 66%, 49%)",
+      violet: {
+        dark: "hsl(257, 27%, 26%)",
+        "very-dark": "hsl(260, 8%, 14%)",
+      },
+      red: "hsl(0, 87%, 67%)",
+      gray: {
+        default: "hsl(0, 0%, 75%)",
+        violet: "hsl(257, 7%, 63%)",
+      },
+      blue: {
+        "very-dark": "hsl(255, 11%, 22%)",
+      },
+    },
+    fontFamily: {
+      sans: ["Poppins", ...fontFamily.sans],
+    },
+    fontWeight: {
+      medium: 500,
+      bold: 700,
+    },
   },
   variants: {},
   plugins: [],
