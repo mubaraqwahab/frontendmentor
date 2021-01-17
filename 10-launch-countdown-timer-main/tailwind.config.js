6b02e435ca6d92f04fa676df6d9dd470e0d77e63
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-01-17 15:50:21 +0100

    Update tailwind config

--- 10-launch-countdown-timer-main/tailwind.config.js
+++ 10-launch-countdown-timer-main/tailwind.config.js
@@ -1,11 +1,30 @@
+const { colors, fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: ["./*.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ["./*.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+		colors: {
+			transparent: colors.transparent,
+			white: colors.white,
+			black: colors.black,
+			blue: {
+				gray: "hsl(237, 18%, 59%)",
+				"dark-desaturated": "hsl(236, 21%, 26%)",
+				"very-dark": "hsl(235, 16%, 14%)",
+				black: "hsl(234, 17%, 12%)",
+			},
+			red: {
+				soft: "hsl(345, 95%, 68%)",
+			},
+		},
+		fontFamily: {
+			sans: ["Red Hat Text", ...fontFamily.sans],
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
