587ecb4be7a238a77425f4a2cba3c1d908e92bb8
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-08-15 11:24:12 +0100

    add normalize.css script

---
+++ scripts/get-normalize.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Download normalize.css into the current working directory
+
+curl https://raw.githubusercontent.com/necolas/normalize.css/master/normalize.css --output ./normalize.css
