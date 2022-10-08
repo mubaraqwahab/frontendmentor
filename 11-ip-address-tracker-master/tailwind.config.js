fb7e2ea403087990e7e56396361cc2545fbe9aee
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-10-08 21:37:55 +0100

    start basic markup and styling

--- 11-ip-address-tracker-master/tailwind.config.js
+++ 11-ip-address-tracker-master/tailwind.config.js
@@ -1,8 +1,33 @@
+const { colors, fontFamily, fontWeight } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./*.html"],
+	theme: {
+		fontFamily: {
+			sans: ["Rubik", ...fontFamily.sans],
+		},
+		fontWeight: {
+			normal: fontWeight.normal, // 400
+			medium: fontWeight.medium, // 500
+			bold: fontWeight.bold, // 700
+		},
+		// colors: {
+		// 	white: colors.white,
+		// 	black: colors.black,
+		// 	transparent: colors.transparent,
+		// 	gray: {
+		// 		...colors.gray,
+		// 		dark: "hsl(0, 0%, 59%)",
+		// 		"very-dark": "hsl(0, 0%, 17%)",
+		// 	},
+		// },
+		extend: {
+			container: {
+				center: true,
+				padding: "1.5rem",
+			},
+		},
+	},
+	plugins: [],
+};
