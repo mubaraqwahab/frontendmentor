9320f6beb18192b363be9777a898395b79d3bb8d
Mubaraq Wahab <mub.wahab26@gmail.com>
2023-01-19 13:34:01 +0100

    use broad number type

--- 12-calculator-app-main/js/machine.ts
+++ 12-calculator-app-main/js/machine.ts
@@ -1,12 +1,10 @@
 import {assign, createMachine, send} from "xstate"
 
-type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
-
-export function isUnsignedDigit(str: string): str is `${Digit}` {
+export function isUnsignedDigit(str: string): str is `${number}` {
 	return /^\d$/.test(str)
 }
 
-export function isSignedDigit(str: string): str is `-${Digit}` {
+export function isSignedDigit(str: string): str is `${number}` {
 	return /^-\d$/.test(str)
 }
 
